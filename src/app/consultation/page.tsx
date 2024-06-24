a882339c7b4fb4ff6f1e71357e45f72ca7f1c678
Priscillia <selvapriscillia@yahoo.fr>
2024-06-24 23:10:43 +0200

    create json data

--- src/app/consultation/page.tsx
+++ src/app/consultation/page.tsx
@@ -1,14 +1,14 @@
 import Layout from "../ui/layout/layout";
 import Title from "../ui/title/title";
 
+
+
 export default function Page() {
   return (
     <Layout>
       <div className="flex items-center justify-center">
         <Title title="Motifs de consultations" />
       </div>
-
-      <section className="flex flex-col items-center gap-4"></section>
     </Layout>
   );
 }
