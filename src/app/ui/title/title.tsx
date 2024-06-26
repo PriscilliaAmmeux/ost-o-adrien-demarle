3beb50215987d16d9b791e410001c64fc962757e
Priscillia <selvapriscillia@yahoo.fr>
2024-06-26 22:12:44 +0200

    restore title because i need icon in form

--- src/app/ui/title/title.tsx
+++ src/app/ui/title/title.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { IconType } from "react-icons";
 
 interface TitleProps {
   title: string;
+  Icon?: IconType; // Icon is still optional
 }
 
-const Title: React.FC<TitleProps> = ({ title }) => (
-  <h1 className="text-xl font-bold " style={{ color: "var(--blue-color)" }}>
-    {title}
-  </h1>
+const Title: React.FC<TitleProps> = ({ title, Icon }) => (
+  <div className="flex items-center mb-4">
+    {Icon && <Icon className=" text-xl mr-2" />}
+    <h1 className="text-xl font-bold " style={{ color: "var(--blue-color)" }}>
+      {title}
+    </h1>
+  </div>
 );
 
 export default Title;
