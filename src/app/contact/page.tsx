c6b3b6e655dbe39cff6fd64ba9f0cf274f797315
Priscillia <selvapriscillia@yahoo.fr>
2024-06-27 11:11:13 +0200

    refacto code

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,15 +6,8 @@ import Layout from "../ui/layout/layout";
 export default function Page() {
   return (
     <Layout>
-      <section className="pt-10 pb-10">
-        <div className="flex flex-col md:flex-row justify-around items-center md:items-start space-y-6 md:space-y-0">
-          <div className="w-full md:w-2/4">
-            <Form />
-          </div>
-          <div className="w-full md:w-1/4">
-            <InfoContact />
-          </div>
-        </div>
+      <section className="pt-10 pb-10 w-full md:w-1/2 mx-auto">
+        <Form />
       </section>
     </Layout>
   );
