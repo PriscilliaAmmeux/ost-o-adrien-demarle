7ff8041449b48ff26aef6156fb3a5979c639ebbd
Priscillia <selvapriscillia@yahoo.fr>
2024-06-27 13:53:22 +0200

    refacto

---
+++ src/app/ui/itemsContact/itemsContact.tsx
@@ -0,0 +1,30 @@
+import Title from "../title/title";
+import { IconType } from "react-icons";
+
+interface ItemsContactProps {
+  title: string;
+  info: { title: string; text: string }[];
+  children?: React.ReactNode;
+  Icon?: IconType;
+}
+
+export default function ItemsContact({
+  title,
+  info,
+  children,
+  Icon,
+}: ItemsContactProps) {
+  return (
+    <section className="flex-1 p-4 md:p-8 mr-4">
+      <Title title={title} Icon={Icon} />
+      {children}
+      <ul>
+        {info.map((item) => (
+          <li key={item.title} className="mb-2">
+            <strong>{item.title}:</strong> {item.text}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
--- src/app/ui/title/title.tsx
+++ src/app/ui/title/title.tsx
@@ -8,8 +8,12 @@ interface TitleProps {
 
 const Title: React.FC<TitleProps> = ({ title, Icon }) => (
   <div className="flex items-center mb-4">
-    {Icon && <Icon className=" text-xl mr-2" />}
-    <h1 className="text-xl font-bold uppercase " style={{ color: "var(--blue-color)" }}>
+    {Icon && (
+      <Icon className=" text-xl mr-2" style={{ color: "var(--blue-color)" }} />
+    )}
+    <h1
+      className="text-xl font-bold uppercase "
+      style={{ color: "var(--blue-color)" }}>
       {title}
     </h1>
   </div>
