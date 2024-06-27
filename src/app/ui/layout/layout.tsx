876961e96abaad0bfc8a07e5a68f611772fc1fbf
Priscillia <selvapriscillia@yahoo.fr>
2024-06-27 09:48:50 +0200

    add info rgpd

--- src/app/ui/layout/layout.tsx
+++ src/app/ui/layout/layout.tsx
@@ -1,17 +1,30 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Footer from "../footer/footer";
 import Navigation from "../navigation/navigation";
+import RgpdModal from "../rgpd/rgpdModal";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
+  const [isRgpdModalOpen, setRgpdModalOpen] = useState(false);
+
+  const handleRgpdClick = () => {
+    setRgpdModalOpen(true);
+  };
+
+  const closeRgpdModal = () => {
+    setRgpdModalOpen(false);
+  };
   return (
     <section>
       <Navigation />
       {children}
-      <Footer />
+      <Footer onRgpdClick={handleRgpdClick} />
+      {isRgpdModalOpen && <RgpdModal onClose={closeRgpdModal} />}
     </section>
   );
 }
---
+++ src/app/ui/rgpd/rgpdModal.tsx
@@ -0,0 +1,30 @@
+import Button from "../button/button";
+import Title from "../title/title";
+
+interface FooterProps {
+  onClose: () => void;
+}
+
+export default function RgpdModal({ onClose }: FooterProps) {
+  return (
+    <section className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="bg-white p-6 rounded-lg shadow-lg w-80 text-center">
+        <Title title="Politique de Confidentialité" />
+        <p className="mb-4">
+          Lors de l&apos;envoi du formulaire de contact, les données conservées
+          le sont à des fins professionnelles dans le cadre de mon activité
+          seulement.
+        </p>
+        <p className="mb-4">
+          Les données ne sont pas utilisées à des fins commerciales, ni
+          transmises à des tiers.
+        </p>
+        <p className="mb-4">
+          Vous pouvez à tout moment demander la suppression de vos données en me
+          contactant par mail.
+        </p>
+        <Button onClick={onClose} type={"button"} text={"Fermer"} />
+      </div>
+    </section>
+  );
+}
