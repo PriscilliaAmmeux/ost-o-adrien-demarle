8fb8d774a950198507123e9905a7ff508caaa20b
Priscillia <selvapriscillia@yahoo.fr>
2024-06-27 11:11:13 +0200

    refacto code

--- src/app/ui/infoContact/infoContact.tsx
+++ src/app/ui/infoContact/infoContact.tsx
@@ -1,12 +1,28 @@
 const contactDetails = {
   contactNumber: "06 37 05 96 79 (mon numéro direct)",
-  openingHours: "du lundi au dimanche de 8h à 21h.",
+  openingHours: "du lundi au samedi de 8h à 21h.",
   nearbyCities:
     "Deûlémont, Wervicq Sud, Wervicq, Quesnoy-sur-Deule, Warneton, Bousbecque, Linselles, Frelinghien, Comines, Armentières, Verlinghem",
-
-  publicParking: "8 Rue du Vieil Dieu, Comines.",
-  practicalInfo: "Rez-de-chaussée.",
 };
+
+const infoDetails = [
+  {
+    title: "Parking public",
+    text: "8 Rue du Vieil Dieu, Comines.",
+  },
+  {
+    title: "Informations pratiques",
+    text: "Rez-de-chaussée.",
+  },
+  {
+    title: "Adeli",
+    text: "590011581",
+  },
+  {
+    title: "Siret",
+    text: "84130121100014",
+  },
+];
 
 export default function InfoContact() {
   return (
@@ -36,19 +52,13 @@ export default function InfoContact() {
       <section
         className="flex-1 p-4 md:p-8 bg-white shadow rounded-lg"
         style={{ color: "var(--black-color)", width: "300", height: "350" }}>
-        <p className="mb-2">
-          <strong>Parking public :</strong> {contactDetails.publicParking}
-        </p>
-        <p className="mb-2">
-          <strong>Informations pratiques :</strong>{" "}
-          {contactDetails.practicalInfo}
-        </p>
-        <p className="mb-2">
-          <strong>Adeli</strong> :590011581
-        </p>
-        <p className="mb-2">
-          <strong>Siret</strong> : 84130121100014{" "}
-        </p>
+        <ul>
+          {infoDetails.map((info) => (
+            <li key={info.title} className="mb-2">
+              <strong>{info.title}:</strong> {info.text}
+            </li>
+          ))}
+        </ul>
       </section>
     </section>
   );
