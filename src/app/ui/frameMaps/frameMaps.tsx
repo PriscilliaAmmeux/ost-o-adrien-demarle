e7ac5f90604f7b70e35a7663b8e686cc22c303cd
Priscillia <selvapriscillia@yahoo.fr>
2024-06-27 10:16:45 +0200

    change footer layout following customer request

--- src/app/ui/frameMaps/frameMaps.tsx
+++ src/app/ui/frameMaps/frameMaps.tsx
@@ -2,7 +2,7 @@ export default function FrameMaps() {
   return (
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2523.713424027585!2d3.01002857608018!3d50.76234726485839!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47dcd36546a27ce3%3A0x971b658801406311!2sAdrien%20Demarle%20Ost%C3%A9opathe!5e0!3m2!1sfr!2sfr!4v1719259786410!5m2!1sfr!2sfr"
-      width="600"
+      width="100%"
       height="350"
       style={{ border: 0 }}
       allowFullScreen={true}
--- src/app/ui/infoContact/infoContact.tsx
+++ src/app/ui/infoContact/infoContact.tsx
@@ -1,29 +1,29 @@
 const contactDetails = {
   contactNumber: "06 37 05 96 79 (mon numéro direct)",
-  email: "demarle.adrien.osteopathe@gmail.com",
   openingHours: "du lundi au dimanche de 8h à 21h.",
   nearbyCities:
-    "Deûlémont, Wervicq Sud, Wervicq, Quesnoy-sur-Deule, Warneton, Bousbecque",
-  transport: [
-    "Bus - Place De Gaulle (lignes L90 et 82).",
-    "Bus - Vieil Dieu (lignes L90 et 84)",
-    "Bus - 3 Ballots (lignes 82, 84 et 86)",
-  ],
+    "Deûlémont, Wervicq Sud, Wervicq, Quesnoy-sur-Deule, Warneton, Bousbecque, Linselles, Frelinghien, Comines, Armentières, Verlinghem",
+
   publicParking: "8 Rue du Vieil Dieu, Comines.",
   practicalInfo: "Rez-de-chaussée.",
 };
 
 export default function InfoContact() {
   return (
-    <div
-      style={{ color: "var(--black-color)", width: "600", height: "350" }}
-      className="p-4 md:p-8 bg-white shadow rounded-lg">
-      <div className="mb-6">
+    <section className="flex flex-wrap">
+      <section
+        className="flex-1 p-4 md:p-8 bg-white shadow rounded-lg mr-4"
+        style={{ color: "var(--black-color)", width: "300", height: "350" }}>
         <p className="mb-2">
           <strong>Contact:</strong> {contactDetails.contactNumber}
         </p>
         <p className="mb-2">
-          <strong>Email:</strong> {contactDetails.email}
+          <strong>Email:</strong>{" "}
+          <a
+            href="mailto:demarle.adrien.osteopathe@gmail.com"
+            className="hover:font-bold hover:text-blue-900">
+            demarle.adrien.osteopathe@gmail.com
+          </a>
         </p>
         <p className="mb-2">
           <strong>Horaires d&apos;ouverture du cabinet :</strong>{" "}
@@ -32,16 +32,10 @@ export default function InfoContact() {
         <p>
           <strong>Proximités villes :</strong> {contactDetails.nearbyCities}
         </p>
-      </div>
-      <div>
-        <p className="mb-2">
-          <strong>Moyens de transport :</strong>
-        </p>
-        <ul className="list-disc pl-5 mb-2">
-          {contactDetails.transport.map((line, index) => (
-            <li key={index}>{line}</li>
-          ))}
-        </ul>
+      </section>
+      <section
+        className="flex-1 p-4 md:p-8 bg-white shadow rounded-lg"
+        style={{ color: "var(--black-color)", width: "300", height: "350" }}>
         <p className="mb-2">
           <strong>Parking public :</strong> {contactDetails.publicParking}
         </p>
@@ -55,7 +49,7 @@ export default function InfoContact() {
         <p className="mb-2">
           <strong>Siret</strong> : 84130121100014{" "}
         </p>
-      </div>
-    </div>
+      </section>
+    </section>
   );
 }
