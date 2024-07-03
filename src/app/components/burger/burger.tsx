bdd790f5f092a20c591757115650b87bf36c9894
Priscillia <selvapriscillia@yahoo.fr>
2024-07-03 20:49:09 +0200

    burger menu responsive

--- src/app/components/burger/burger.tsx
+++ src/app/components/burger/burger.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { FaBars, FaTimes } from "react-icons/fa";
-
 import styles from "../../styles/burger.module.css";
 import { useState } from "react";
 import NavLink from "@/app/ui/navLink/navLink";
@@ -12,10 +11,17 @@ export default function Burger() {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+
+  const handleCloseMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <section>
       <div className="flex justify-between items-center pt-4 pb-4">
-        <h1 className="text-2xl font-bold uppercase pl-2">Stéphanie HEUDRE</h1>
+        <h1 className="text-2xl font-bold uppercase pl-2">
+          Adrien Demarle - Ostéophate DO - Comines
+        </h1>
         <button
           className="lg:hidden"
           onClick={toggleMenu}
@@ -25,14 +31,27 @@ export default function Burger() {
       </div>
 
       <div className={`${styles.burgerMenu} ${isOpen ? styles.open : ""}`}>
-        <NavLink href="/" title="Accueil" isActive={true} />
+        <NavLink
+          href="/"
+          title="Accueil"
+          onClick={handleCloseMenu}
+          isActive={true}
+          className="text-lg ml-10"
+        />
         <NavLink
           href="/consultation"
           title="Motifs de consultations"
+          onClick={handleCloseMenu}
           isActive={true}
+          className="text-lg pl-4"
         />
-        <NavLink href="/price" title="Tarifs" isActive={true} />
-        <NavLink href="/contact" title="Contact" isActive={true} />
+        <NavLink
+          href="/price"
+          title="Tarifs"
+          onClick={handleCloseMenu}
+          isActive={true}
+          className="text-lg"
+        />
       </div>
     </section>
   );
