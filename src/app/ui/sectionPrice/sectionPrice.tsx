13a86b7779928e4a54e1446ba063a472c1a796ef
Priscillia <selvapriscillia@yahoo.fr>
2024-07-04 14:22:21 +0200

    change bg color to component section price

--- src/app/ui/sectionPrice/sectionPrice.tsx
+++ src/app/ui/sectionPrice/sectionPrice.tsx
@@ -1,17 +1,13 @@
 interface SectionPriceProps {
-  bgColor: string;
   title: string;
   children: React.ReactNode;
 }
 
-export default function SectionPrice({
-  bgColor,
-  title,
-  children,
-}: SectionPriceProps) {
+export default function SectionPrice({ title, children }: SectionPriceProps) {
   return (
     <section
-      className={`${bgColor} flex-1 p-6 rounded shadow-md text-white mb-4 md:mb-0`}>
+      className="flex-1 p-6 rounded shadow-md mb-4 md:mb-0"
+      style={{ backgroundColor: "var(--blue-color-light)" }}>
       <h2 className="font-bold text-lg mb-2" style={{ fontFamily: "cursive" }}>
         {title}
       </h2>
