a08415a73799ea53fc3b1633409c268cc858dc8c
Priscillia <selvapriscillia@yahoo.fr>
2024-07-04 13:37:26 +0200

    add observer to logo

--- src/app/ui/logo/logo.tsx
+++ src/app/ui/logo/logo.tsx
@@ -1,12 +1,40 @@
+"use client";
+
+import React, { useState, useEffect, useRef } from "react";
+
 interface LogoProps {
   width: number;
 }
 
 export default function Logo({ width }: LogoProps = { width: 100 }) {
+  const [isVisible, setIsVisible] = useState(false);
+  const imgRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect();
+        }
+      },
+      { threshold: 0.01 }
+    );
+
+    if (imgRef.current) {
+      observer.observe(imgRef.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
     <section>
       <img
-        src="/logo.webp"
+        ref={imgRef}
+        src={isVisible ? "/logo.webp" : undefined}
         loading="lazy"
         alt="logo d'entreprise (une colonne vertébrale stylisée en forme de S)"
         width={width}
