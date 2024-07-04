0166db1db6ad34d9c0ff6b912e83e1a221d61d5e
Priscillia <selvapriscillia@yahoo.fr>
2024-07-04 10:36:53 +0200

    fix logo mobile

--- src/app/ui/logo/logo.tsx
+++ src/app/ui/logo/logo.tsx
@@ -7,10 +7,10 @@ export default function Logo({ width }: LogoProps = { width: 100 }) {
     <section>
       <img
         src="/logo.webp"
-        srcSet="/logo-small.webp 500w, /logo-medium.webp 1000w, /logo-large.webp 1500w"
-        sizes="(max-width: 600px) 500px, (max-width: 1200px) 1000px, 1500px"
+        loading="lazy"
         alt="logo d'entreprise (une colonne vertébrale stylisée en forme de S)"
         width={width}
+        style={{ maxWidth: "100%", height: "auto" }}
       />
     </section>
   );
