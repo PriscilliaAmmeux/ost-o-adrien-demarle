fd12540e0937d4f046d6df59e433a8d05e2fd4a9
Priscillia <selvapriscillia@yahoo.fr>
2024-07-04 13:52:02 +0200

    replace image by next

--- src/app/ui/logo/logo.tsx
+++ src/app/ui/logo/logo.tsx
@@ -1,45 +1,19 @@
-"use client";
-
-import React, { useState, useEffect, useRef } from "react";
+import Image from "next/image";
 
 interface LogoProps {
   width: number;
 }
 
-export default function Logo({ width }: LogoProps = { width: 100 }) {
-  const [isVisible, setIsVisible] = useState(false);
-  const imgRef = useRef<HTMLImageElement>(null);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        if (entries[0].isIntersecting) {
-          setIsVisible(true);
-          observer.disconnect();
-        }
-      },
-      { threshold: 0.01 }
-    );
-
-    if (imgRef.current) {
-      observer.observe(imgRef.current);
-    }
-
-    return () => {
-      observer.disconnect();
-    };
-  }, []);
-
+export default function Logo({ width }: LogoProps) {
+  const height = (width / 16) * 9;
   return (
-    <section>
-      <img
-        ref={imgRef}
-        src={isVisible ? "/logo.webp" : undefined}
-        loading="lazy"
-        alt="logo d'entreprise (une colonne vertébrale stylisée en forme de S)"
-        width={width}
-        style={{ maxWidth: "100%", height: "auto" }}
-      />
-    </section>
+    <Image
+      src="/logo.webp"
+      loading="lazy"
+      alt="logo d'entreprise (une colonne vertébrale stylisée en forme de S)"
+      width={width}
+      height={height}
+      style={{ maxWidth: "100%", height: "auto" }}
+    />
   );
 }
