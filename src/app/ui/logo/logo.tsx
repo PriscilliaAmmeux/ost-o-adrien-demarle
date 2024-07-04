98200188520431ef9c8b9a4d87c580239728bf25
Priscillia <selvapriscillia@yahoo.fr>
2024-07-04 10:28:36 +0200

    resize img

--- src/app/ui/logo/logo.tsx
+++ src/app/ui/logo/logo.tsx
@@ -7,6 +7,8 @@ export default function Logo({ width }: LogoProps = { width: 100 }) {
     <section>
       <img
         src="/logo.webp"
+        srcSet="/logo-small.webp 500w, /logo-medium.webp 1000w, /logo-large.webp 1500w"
+        sizes="(max-width: 600px) 500px, (max-width: 1200px) 1000px, 1500px"
         alt="logo d'entreprise (une colonne vertébrale stylisée en forme de S)"
         width={width}
       />
