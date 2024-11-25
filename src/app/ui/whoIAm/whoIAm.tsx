cdd1ef98cdba09247f29925c9624bf097b039cbd
Priscillia <selvapriscillia@yahoo.fr>
2024-11-25 14:51:58 +0100

    fix error vercel

--- src/app/ui/whoIAm/whoIAm.tsx
+++ src/app/ui/whoIAm/whoIAm.tsx
@@ -5,13 +5,14 @@ export default function () {
     <section>
       <Subtitle subtitle="Qui suis-je ?" />
       <p className="text-justify">
-        Étant petit, mes parents m'avaient emmené voir un acupuncteur, à partir
-        de ce moment, j'ai eu un intérêt grandissant pour ce que l'on nomme
-        complémentaire. Suite à une scoliose, j'ai fait du kiné avec un kiné qui
-        faisait aussi de l'ostéopathie, grâce à lui j'avais voulu avoir la
-        double compétence. Aujourd'hui, j'exerce l'ostéopathie et j'adore ce que
-        je fais. En plus de la pratique au cabinet, j'interviens aussi en
-        entreprise ou je prends plaisir à faire découvrir l'ostéopathie!
+        Étant petit, mes parents m&apos;avaient emmené voir un acupuncteur, à
+        partir de ce moment, j&apos;ai eu un intérêt grandissant pour ce que
+        l&apos;on nomme complémentaire. Suite à une scoliose, j&apos;ai fait du
+        kiné avec un kiné qui faisait aussi de l&apos;ostéopathie, grâce à lui
+        j&apos;avais voulu avoir la double compétence. Aujourd&apos;hui,
+        j&apos;exerce l&apos;ostéopathie et j&apos;adore ce que je fais. En plus
+        de la pratique au cabinet, j&apos;interviens aussi en entreprise ou je
+        prends plaisir à faire découvrir l&apos;ostéopathie!
       </p>
     </section>
   );
--- src/app/ui/whyToConsult/whyToConsult.tsx
+++ src/app/ui/whyToConsult/whyToConsult.tsx
@@ -5,32 +5,34 @@ export default function WhyToConsult() {
     <section>
       <SectionHomePage subtitle="Vous cherchez une solution pour des douleurs comme un mal de dos, sciatique...?">
         <p className="text-justify">
-          L'ostéopathie peut apporter un soulagement, aussi bien pour le dos que
-          pour le reste du corps! En effet en libérant des tensions et des
-          blocages, il est possible d'atténuer des douleurs comme une sciatique,
-          mal de dos, cruralgie, tendinite, entorse, névralgie, lumbago etc
+          L&apos;ostéopathie peut apporter un soulagement, aussi bien pour le
+          dos que pour le reste du corps! En effet en libérant des tensions et
+          des blocages, il est possible d'atténuer des douleurs comme une
+          sciatique, mal de dos, cruralgie, tendinite, entorse, névralgie,
+          lumbago etc
         </p>
       </SectionHomePage>
       <SectionHomePage subtitle="Qu'est-ce que l'ostéopathie ?">
         <ul className="text-justify">
           <li>
-            L'ostéopathie est une méthode de soin manuelle et naturelle par
-            laquelle l'ostéopathe va rechercher les différentes restrictions de
-            mobilités au sein du corps.
+            L&apos;ostéopathie est une méthode de soin manuelle et naturelle par
+            laquelle l&apos;ostéopathe va rechercher les différentes
+            restrictions de mobilités au sein du corps.
           </li>
           <li>
             Une perte de mobilité engendre un déséquilibre dans la santé du
             patient.
           </li>
           <li>
-            L'ostéopathie s'appuie sur le principe que l'etre humain est une
-            unité fonctionnelle qui possède des mécanismes d'autorégulation et
-            que la structure et la fonction sont en lien.
+            &apos;ostéopathie s&apos;appuie sur le principe que l&apos;etre
+            humain est une unité fonctionnelle qui possède des mécanismes
+            d&apos;autorégulation et que la structure et la fonction sont en
+            lien.
           </li>
           <li>
-            Il est à noter que l'ostéopathie est une thérapie de première
-            intention, c'est-à-dire sans nécessité de passer par un médecin au
-            préalable.
+            Il est à noter que l&apos;ostéopathie est une thérapie de première
+            intention, c&apos;est-à-dire sans nécessité de passer par un médecin
+            au préalable.
           </li>
         </ul>
       </SectionHomePage>
