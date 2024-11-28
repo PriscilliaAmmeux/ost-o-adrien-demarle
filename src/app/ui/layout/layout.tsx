0c11d7c38d57f9fb1fc52da46ec7c25447188516
Priscillia <selvapriscillia@yahoo.fr>
2024-11-28 17:41:59 +0100

    call phone number

--- src/app/ui/layout/layout.tsx
+++ src/app/ui/layout/layout.tsx
@@ -22,7 +22,7 @@ export default function Layout({ children }: LayoutProps) {
       {children}
       <Footer />
       <a
-        href="tel:+33666924101"
+        href="tel:+33637059679"
         className="fixed bottom-4 right-4 bg-blue-500 text-white p-4 rounded-full shadow-lg md:hidden"
         style={{ zIndex: 1000 }}>
         {" "}
