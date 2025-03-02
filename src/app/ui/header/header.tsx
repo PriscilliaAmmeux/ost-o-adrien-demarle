e4620fec0c3428554f13722910e72662e310657a
Priscillia <selvapriscillia@yahoo.fr>
2025-03-02 16:25:52 +0100

    fix header v3

--- src/app/ui/header/header.tsx
+++ src/app/ui/header/header.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import Appointment from "../appointment/appointment";
+import Button from "../button/button";
 
 export default function Header() {
   return (
@@ -19,19 +20,24 @@ export default function Header() {
         quality={100}
         priority
       />
-      <div className="absolute flex md:flex-row flex-col justify-between items-center w-full h-full">
-        <section className="flex flex-col justify-center items-center w-full h-full md:pl-10 text-center md:text-left md:w-1/2">
+      <div className="absolute flex md:flex-row flex-col w-full h-full">
+        <section className="flex flex-col justify-center w-full h-full md:pl-10 text-center md:text-left md:w-2/3">
           <h2 className="text-4xl font-bold text-white pb-5">
-            Adrien Demarle Ostéopathe
+            Adrien Demarle Ostéopathe à Comines
           </h2>
-          <div className="gap-10 justify-center items-center">
+          <div>
             <Appointment />
+            <Button
+              type={"button"}
+              text={"06 37 05 96 79"}
+              ariaLabel="Mon numéro de téléphone : 0637059679"
+              className="md:ml-5 md:mt-0 mt-1"
+            />
           </div>
         </section>
 
-        <section className="flex flex-col justify-center w-full text-xl font-bold text-white pb-5 text-center md:text-left md:w-1/2">
+        <section className="flex flex-col justify-center w-full text-xl font-bold text-white pb-5 text-center md:text-left md:w-1/3 md:ml-10">
           <p>Adresse: 47 Rue Marceau, 59560 Comines</p>
-          <p>Contact: 06 37 05 96 79 </p>
         </section>
       </div>
     </header>
