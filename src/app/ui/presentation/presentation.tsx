356e95bca936f4e54bf1aabfeaf66a414a321c70
Priscillia <selvapriscillia@yahoo.fr>
2025-03-06 12:39:16 +0100

    fix error vercel and dispo presentation

--- src/app/ui/presentation/presentation.tsx
+++ src/app/ui/presentation/presentation.tsx
@@ -8,17 +8,6 @@ export default function Presentation() {
     <section
       style={{ background: "var(--blue-color-light)" }}
       className="w-full flex flex-col md:flex-row items-center font-sans text-gray-800 mt-10 md:mb-10 pt-5 pb-5">
-      <div className="w-full md:w-1/3">
-        <Image
-          src={"/consult.webp"}
-          alt="photo de manipulation d'ostéopathie"
-          className="rounded-xl flex-shrink-0 shadow-lg mx-auto md:ml-5 md:mr-5"
-          security="restricted"
-          width={300}
-          height={300}
-        />
-      </div>
-
       <div className="w-full md:w-2/3 md:ml-10 px-5 md:px-0">
         <h1
           className="text-2xl md:text-4xl font-bold mb-5 mt-5 flex flex-col"
@@ -33,6 +22,16 @@ export default function Presentation() {
         <div className="mt-5">
           <Appointment />
         </div>
+      </div>
+      <div className="w-full md:w-1/3">
+        <Image
+          src={"/consult.webp"}
+          alt="photo de manipulation d'ostéopathie"
+          className="rounded-xl flex-shrink-0 shadow-lg mx-auto md:ml-5 md:mr-5"
+          security="restricted"
+          width={300}
+          height={300}
+        />
       </div>
     </section>
   );
