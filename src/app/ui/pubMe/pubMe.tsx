694c7a96eeebc6420e07aceee732a96f31e83618
Priscillia <selvapriscillia@yahoo.fr>
2025-10-13 21:32:48 +0200

    replace name company and update hover mail

--- src/app/ui/pubMe/pubMe.tsx
+++ src/app/ui/pubMe/pubMe.tsx
@@ -2,16 +2,13 @@ export default function PubMe() {
   return (
     <section className="flex flex-col md:flex-row gap-2 items-center justify-center pb-20 md:pb-0">
       <p className="italic">
-        Créé par <span className="font-bold">Priscillia Ammeux</span>
-      </p>
-      <p className="flex flex-col md:flex-row items-center justify-center">
-        Conceptrice et Développeuse d&apos;Applications:
+        Créé par{" "}
         <a
-          href="http://www.priscillia-ammeux-portfolio.fr"
-          className="pl-2 hover:underline hover:font-semibold"
+          href="https://www.pixelia-and-co.fr/"
+          className="font-bold hover:underline hover:font-semibold"
           target="_blank"
           rel="noopener noreferrer">
-          www.priscillia-ammeux-portfolio.fr
+          Pixelia&Co
         </a>
       </p>
     </section>
